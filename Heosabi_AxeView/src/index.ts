2e07f21794f4d4eff2f7b5afe7e96626e874e200
dang-gun <c1dong8@gmail.com>
2024-03-07 17:09:25 +0900

    프로젝트 구성 변경 및 생성

---
+++ Heosabi_AxeView/src/index.ts
@@ -0,0 +1,16 @@
+﻿
+export default class App
+{
+	/** 지금 보여주고 있는 페이지에서 사용할 개체 */
+	public PageNow: any = null;
+
+	constructor()
+	{
+		//메뉴 추가
+		let divMain = document.getElementById("divMain");
+		divMain.innerHTML = "Test";
+	}
+
+}
+
+(window as any).app = new App();
